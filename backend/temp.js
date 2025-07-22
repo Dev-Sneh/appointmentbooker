d7ab87fb341cfb5492fdcf0317236a781afffbda
Sneh Agrawal <69115172+Dev-Sneh@users.noreply.github.com>
2025-07-22 11:27:58 +0530

    Update temp.js

--- backend/temp.js
+++ backend/temp.js
@@ -1,5 +1,1 @@
-// Run this in a Node.js file to convert service account JSON into a string
-const fs = require('fs');
-const serviceAccount = require('./firebase/serviceAccountKey.json');
-
-console.log(JSON.stringify(serviceAccount));
+temp
